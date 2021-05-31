9c38ce04e963716312cbafccd43716ca0c274308
Sudarshan Gosavi <sudarshangosavi17@gmail.com>
2021-05-31 14:54:56 +0530

    Created infrastucture and added to the app level

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
+import { ThemeProvider } from "styled-components";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { theme } from "./src/infrastucture/theme";
 
 export default function App() {
   return (
     <>
-      <RestaurantsScreen />
-      <ExpoStatusBar style="auto" />
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+        <ExpoStatusBar style="auto" />
+      </ThemeProvider>
     </>
   );
 }
