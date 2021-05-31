09faac44a33117bff430aad23b4809936f2585e9
Sudarshan Gosavi <sudarshangosavi17@gmail.com>
2021-05-31 14:37:35 +0530

    Implemented Styled-Component

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,38 +1,31 @@
 import React from "react";
-import {
-  Platform,
-  SafeAreaView,
-  StyleSheet,
-  View,
-  StatusBar,
-} from "react-native";
+import { Platform, SafeAreaView, View, StatusBar } from "react-native";
 import { Searchbar } from "react-native-paper";
+import styled from "styled-components";
 import { RestaurantInfo } from "../components/restaurant-info-card.component";
 
 const isAndroid = Platform.OS === "android";
 
-export const RestaurantsScreen = () => (
-  <SafeAreaView style={styles.container}>
-    <View style={styles.search}>
-      <Searchbar />
-    </View>
-    <View style={styles.list}>
-      <RestaurantInfo />
-    </View>
-  </SafeAreaView>
-);
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  margin-top: ${isAndroid ? StatusBar.currentHeight : 0}px;
+`;
+const SearchContainer = styled(View)`
+  padding: 16px;
+`;
+const RestaurantListContainer = styled(View)`
+  flex: 1;
+  padding: 16px;
+  background-color: blue;
+`;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: isAndroid ? StatusBar.currentHeight : 0,
-  },
-  search: {
-    padding: 16,
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "blue",
-  },
-});
+export const RestaurantsScreen = () => (
+  <SafeArea>
+    <SearchContainer>
+      <Searchbar />
+    </SearchContainer>
+    <RestaurantListContainer>
+      <RestaurantInfo />
+    </RestaurantListContainer>
+  </SafeArea>
+);
