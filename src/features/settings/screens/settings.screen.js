83c5365046ef4ba7cb330164193f4ab59b9a1ce8
Sudarshan Gosavi <sudarshangosavi17@gmail.com>
2021-11-26 12:51:17 +0530

    Profile Picture Feature Added

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,11 +1,15 @@
-import React, { useContext } from "react";
+import React, { useCallback, useContext, useState } from "react";
+import { TouchableOpacity } from "react-native";
 import { List, Avatar } from "react-native-paper";
 import styled from "styled-components";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { Text } from "../../../components/typography/text.component";
 
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { AuthContext } from "../../../services/authentication/authentication.context";
+import { useFocusEffect } from "@react-navigation/native";
 
 const SettingsItem = styled(List.Item)`
   padding: ${(props) => props.theme.space[3]};
@@ -17,10 +21,34 @@ const AvatarContainer = styled.View`
 
 export const SettingsScreen = ({ navigation }) => {
   const { onLogout, user } = useContext(AuthContext);
+  const [photo, setPhoto] = useState(null);
+
+  const getProfilePicture = async (currentUser) => {
+    const photoUri = await AsyncStorage.getItem(`${currentUser.uid}-photo`);
+    setPhoto(photoUri);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      getProfilePicture(user);
+    }, [user])
+  );
+
   return (
     <SafeArea>
       <AvatarContainer>
-        <Avatar.Icon size={100} icon="account" backgroundColor="#2182BD" />
+        <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
+          {!photo && (
+            <Avatar.Icon size={100} icon="account" backgroundColor="#2182BD" />
+          )}
+          {photo && (
+            <Avatar.Image
+              size={100}
+              source={{ uri: photo }}
+              backgroundColor="#2182BD"
+            />
+          )}
+        </TouchableOpacity>
         <Spacer position="top" size="large">
           <Text variant="caption">{user.email}</Text>
         </Spacer>
